09023c7888855c4a9d6c2b072825555e571efeb6
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 17:37:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,17 @@
 def create_an_empty_array
-  
+    [ ]
 end
 
 def create_an_array
-  
+    ["goat", "cow", "chicken", "pig" ]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   array = [ ]
+   element = array.push
 end
+add_element_to_end_of_array(["john", "steve"], element = array << "william")
+
 
 def add_element_to_start_of_array(array, element)
   
