cff4372e61a9b29610c8d0efb3c228c6ba6df5d1
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 17:46:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 add_element_to_start_of_array(["wow", "I", "am", "really", "learning"], "wow")
 
 def remove_element_from_end_of_array(array)
-  array = [ ]
+  array = ["I", "am", "really", "learning", "arrays!"]
 end
 remove_element_from_end_of_array(array.pop)
 
